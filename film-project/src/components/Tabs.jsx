d5a86f89207fbdb56068c6c8e31c7db39e69e6a4
SadiqAgayev <sadiqagayev820@gmail.com>
2024-01-09 11:40:01 +0400

    added active class in tabs

--- film-project/src/components/Tabs.jsx
+++ film-project/src/components/Tabs.jsx
@@ -1,31 +1,41 @@
-"use client"
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
 const Tabs = () => {
-    const tabs = [
-        {
-            name: "En Populyar",
-            url: 'popular'
-        },
-        {
-            name: "En Son",
-            url: 'latest'
-        },
-        {
-            name: "Yaxinda Gelecekler",
-            url: 'upcoming'
-        },
-    ]
+  const tabs = [
+    {
+      name: "En Populyar",
+      url: "popular",
+    },
+    {
+      name: "En Son",
+      url: "latest",
+    },
+    {
+      name: "Yaxinda Gelecekler",
+      url: "upcoming",
+    },
+  ];
+  const getQuery = useSearchParams().get("genre");
   return (
-    <div className='p-5 my-5 bg-gray-100 dark:bg-gray-800 flex items-center justify-center gap-7'>
-        {
-            tabs.map((tab,i) => {
-                <Link className='cursor-pointer hover:opacity-75 transition-opacity' key={i} href={``}>{tab.name}</Link>
-            })
-        }
+    <div className="p-5 my-5 bg-gray-100 dark:bg-gray-800 flex items-center justify-center gap-7">
+      {tabs.map((tab, i) => (
+        <Link
+          className={`cursor-pointer hover:opacity-75 transition-opacity ${
+            tab.url === getQuery
+              ? "underline underline-offset-8 text-amber-600"
+              : ""
+          }`}
+          key={i}
+          href={`?genre=${tab.url}`}
+        >
+          {tab.name}
+        </Link>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Tabs
+export default Tabs;
